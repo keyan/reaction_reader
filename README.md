7f1f0cb49c5a514d57d962768485f16d737e970c
Keyan Pishdadian <kpishdadian@gmail.com>
2021-05-15 14:00:51 -0700

    Add screenshots

---
+++ README.md
@@ -0,0 +1,10 @@
+# reaction reader
+
+## Setup
+
+```
+brew install mpg123
+pip install -r requirements.txt
+python main.py
+```
+
--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import io
 import os
+import time
 from collections import Counter
+from tempfile import NamedTemporaryFile
 
 import cv2
 import numpy as np
+import pyautogui
 from gtts import gTTS
 from mpg123 import Mpg123, Out123
 
 
-def extract_qr_codes():
-    image = cv2.imread('multi.png')
+def get_screen_image():
+    with NamedTemporaryFile() as f:
+        pil_image = pyautogui.screenshot(imageFilename=f.name)
+
+    opencvImage = cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
+    return opencvImage
+
+
+def extract_qr_codes(image):
     qrCodeDetector = cv2.QRCodeDetector()
     res = qrCodeDetector.detectAndDecodeMulti(image)
     return Counter(res[1])
@@ -23,6 +33,9 @@ def audio_describe(codes):
         elif code == 'smiling':
             text += f'{count} users are smiling, '
 
+    if text == '':
+        return
+
     with io.BytesIO() as f:
         gTTS(text=text, lang='en', slow=False).write_to_fp(f)
         f.seek(0)
@@ -34,5 +47,21 @@ def audio_describe(codes):
 
 
 if __name__ == '__main__':
-    codes_and_counts = extract_qr_codes()
-    audio_describe(codes_and_counts)
+    while True:
+        # Sanity check using a pre-made image.
+        # Comment out the get_screen_image() call to test this.
+        # image = cv2.imread('multi.png')
+
+        s = time.perf_counter()
+        image = get_screen_image()
+        print(f'Screenshot time: {time.perf_counter() - s:0.2f} secs')
+
+        s = time.perf_counter()
+        codes_and_counts = extract_qr_codes(image)
+        print(f'QR extraction time: {time.perf_counter() - s:0.2f} secs')
+        if len(codes_and_counts) > 0:
+            s = time.perf_counter()
+            audio_describe(codes_and_counts)
+            print(f'Audio time: {time.perf_counter() - s:0.2f} secs')
+        else:
+            print('No QR codes detected')
